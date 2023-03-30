098a2c54b0e208a714d296c1f87f840a6ba2a3bb
Mathew Pellarin <mat_p1999@hotmail.com>
2023-03-30 06:51:03 -0400

    finished skills and edu almost done

--- js/barset/js/skillset.js
+++ js/barset/js/skillset.js
@@ -1,97 +1,106 @@
 (function ($) {
-	$.fn.skillset = function (options) {
-		_this = this;
-		$.fn.extend({
-			isOnScreen: function () {
-				var win = $(window);
-				var viewport = {
-					top: win.scrollTop(),
-					left: win.scrollLeft()
-				};
-				viewport.right = viewport.left + win.width();
-				viewport.bottom = viewport.top + win.height();
-				var bounds = this.offset();
-				bounds.right = bounds.left + this.outerWidth();
-				bounds.bottom = bounds.top + this.outerHeight();
+    var instances = [];
 
-				return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-			},
+    $.fn.skillset = function (options) {
+        var _this = this;
 
-			setRun: function (option) {
-				hasRun = option;
-			},
-			checkRun: function () {
-				return hasRun;
-			}
-		});
+        var settings = $.extend({
+            object: "",
+            duration: 80
+        }, options);
 
-		var settings = $.extend({
-			// These are the defaults.
-			object: "",
-			duration: 80
-		}, options);
+        _this.settings = settings;
 
-		$(this).setRun(false);
-		if ($(this).isOnScreen() && !$(this).checkRun()) {
-			create_trigger($(this));
-		}
+        if (_this.isOnScreen() && !_this.hasRun()) {
+            create_trigger(_this);
+        }
 
-		function bar_loop(start, value, length) {
-			var j = 0;
-			for (var i = 0; i < length; i++) {
+        instances.push(_this);
+    };
 
-				setTimeout(function () {
+    $.fn.isOnScreen = function () {
+        var win = $(window);
+        var viewport = {
+            top: win.scrollTop(),
+            left: win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+        var bounds = this.offset();
+        bounds.right = bounds.left + this.outerWidth();
+        bounds.bottom = bounds.top + this.outerHeight();
 
-					if (j < value) {
-						var html = '<div class="full"></div>';
-						$(html).appendTo(start).css('width', 100 / length + '%');
-					} else {
-						var html = '<div class="empty"></div>';
-						$(html).appendTo(start).css('width', 100 / length + '%');
-					}
-					j++;
+        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+    };
 
-				}, settings.duration * i / (length / 10));
-			}
-		}
+    $.fn.setRun = function (option) {
+        this.data('hasRun', option);
+    };
 
-		function create_trigger(element) {
-			var key, count = 0;
-			for (key in settings.object) {
-				if (object.hasOwnProperty(key)) {
-					count++;
-				}
-			}
-			$(element).setRun(true);
-			start = $(element).append('<ul class="skillset-list"></ul>');
+    $.fn.hasRun = function () {
+        return this.data('hasRun');
+    };
 
-			for (var i = 0; i < count; i++) {
+    function bar_loop(start, value, length, duration) {
+        var j = 0;
+        for (var i = 0; i < length; i++) {
+            setTimeout(function () {
+                if (j < value) {
+                    var html = '<div class="full"></div>';
+                    $(html).appendTo(start).css('width', 100 / length + '%');
+                } else {
+                    var html = '<div class="empty"></div>';
+                    $(html).appendTo(start).css('width', 100 / length + '%');
+                }
+                j++;
+            }, duration * i / (length / 10));
+        }
+    }
 
-				start = $(element).find('ul');
-				var html = '<li class="skill-' + (i + 1) + '""><p>' + settings.object[i]['headline'] + ' <span class="icon-info-circled" data-info="' + settings.object[i]['description'] + '"></span></p><div class="bar"></div></li>';
-				html = $(html).appendTo(start).find('.bar');
 
-				var value = settings.object[i]['value'];
-				var length = settings.object[i]['length'];
+    function create_trigger(element) {
+        var settings = element.settings;
+        var key, count = 0;
+        for (key in settings.object) {
+            if (settings.object.hasOwnProperty(key)) {
+                count++;
+            }
+        }
+        $(element).setRun(true);
+        start = $(element).append('<ul class="skillset-list"></ul>');
 
-				bar_loop(html, value, length);
-			}
-		}
-		$(document).scroll(function () {
-			if ($(_this).isOnScreen() && !$(_this).checkRun()) {
-				create_trigger($(_this));
-			}
-		});
+        for (var i = 0; i < count; i++) {
 
-		$('.icon-info-circled').hover(function () {
-			$(this).css('color', '#222222');
-			$(this).parent().parent().append('<div id="list-info" ><p>' + $(this).data('info') + '</p></div>');
-			setTimeout(function () {
-				$('#list-info').css({ 'opacity': 0.9, 'bottom': 50 });
-			}, 100);
-		}, function () {
-			$(this).css('color', 'auto');
-			$('#list-info').remove();
-		});
-	};
+            start = $(element).find('ul');
+            var html = '<li class="skill-' + (i + 1) + '"><p>' + settings.object[i]['headline'] + ' <span class="icon-info-circled" data-info="' + settings.object[i]['description'] + '"></span></p><div class="bar"></div></li>';
+            html = $(html).appendTo(start).find('.bar');
+
+            var value = settings.object[i]['value'];
+            var length = settings.object[i]['length'];
+
+            bar_loop(html, value, length, settings.duration);
+        }
+    }
+
+
+    $(document).scroll(function () {
+        for (var i = 0; i < instances.length; i++) {
+            var instance = instances[i];
+            if (instance.isOnScreen() && !instance.hasRun()) {
+                create_trigger(instance);
+            }
+        }
+    });
+
+    $(document).on('mouseenter', '.icon-info-circled', function () {
+        $(this).css('color', '#222222');
+        $(this).parent().parent().append('<div id="list-info" ><p>' + $(this).data('info') + '</p></div>');
+        setTimeout(function () {
+            $('#list-info').css({ 'opacity': 0.9, 'bottom': 50 });
+        }, 100);
+    }).on('mouseleave', '.icon-info-circled', function () {
+        $(this).css('color', 'auto');
+        $('#list-info').remove();
+    });
 }(jQuery));
+
